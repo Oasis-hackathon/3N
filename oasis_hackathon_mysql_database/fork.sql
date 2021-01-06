da9b147349af2f16f5686cd30f8885777e4acc62
BranKein <6712kyh@naver.com>
2021-01-06 11:54:22 +0900

    For Oasis-Hackathon

--- oasis_hackathon_mysql_database/fork.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists oasis_hackathon_database.fork
-(
-	uuid binary(16) not null,
-	article_id int not null
-);
-
--- oasis_hackathon_mysql_database/user_to_group.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists oasis_hackathon_database.user_to_group
-(
-	user_uuid binary(16) not null,
-	group_id int not null
-);
-
